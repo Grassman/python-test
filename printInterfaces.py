f243a65cbb81f41c2b1dc629c3d4eb3542909a83
Grassman <daniel.gronberg@diabol.se>
2012-11-10 22:00:20 +0100

    Updated printInterfaces to use mysql db

--- printInterfaces.py
+++ printInterfaces.py
@@ -1,17 +1,53 @@
-#!/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import dbus
+import MySQLdb as mdb
+import sys
 
-bus = dbus.SystemBus()
+con = None
+cur = None
 
-proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
-manager = dbus.Interface(proxy, "org.freedesktop.NetworkManager")
+def createTable():
+	cur.execute("CREATE TABLE IF NOT EXISTS Accesspoints(Id INT PRIMARY KEY AUTO_INCREMENT, SSID VARCHAR(255), MAC VARCHAR(255), ENCRYPTED VARCHAR(255))")
 
-# Get device-specific state
-devices = manager.GetDevices()
-for d in devices:
-    dev_proxy = bus.get_object("org.freedesktop.NetworkManager", d)
-    prop_iface = dbus.Interface(dev_proxy, "org.freedesktop.DBus.Properties")
+def persistAp(ssid, mac, encrypted):
+        cur.execute("SELECT * FROM Accesspoints WHERE MAC='"+mac+"'")
+        rows = cur.fetchall()
+	if len(rows) == 0:
+		cur.execute("INSERT INTO Accesspoints(SSID, MAC, ENCRYPTED) VALUES('"+ssid+"','"+mac+"','"+encrypted+"')")
+	else:
+	    	print "AP with MAC='" + mac + "' already added"
 
-    name = prop_iface.Get("org.freedesktop.NetworkManager.Device", "Interface")
-    print 'Interface: ' + name + ' found.' 
+def printApTable():
+	cur.execute("SELECT * FROM Accesspoints")
+	rows=cur.fetchall()
+        print 'Rows found: '
+        for row in rows:
+		print "%2s %3s %4s %5s" % row 
+
+def findAps():
+	bus = dbus.SystemBus()
+
+	proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+	manager = dbus.Interface(proxy, "org.freedesktop.NetworkManager")
+
+	# Get device-specific state
+	devices = manager.GetDevices()
+	#for d in devices:
+    	#	dev_proxy = bus.get_object("org.freedesktop.NetworkManager", d)
+    	#	prop_iface = dbus.Interface(dev_proxy, "org.freedesktop.DBus.Properties")
+	persistAp('ssid','mac','YES');
+	persistAp('ssid2','mac2','NO');
+    	#name = prop_iface.Get("org.freedesktop.NetworkManager.Device", "Interface")
+    	#print 'Interface: ' + name + ' found.'
+ 
+if __name__ == "__main__":
+        con = mdb.connect('localhost', 'test', 'test666', 'test')
+        with con:
+                cur = con.cursor()
+        createTable()
+        findAps()
+	con.commit()
+        printApTable()
+        con.close()
