659821b6bde597b2af577f78a89598f32199a82c
Grassman <daniel.gronberg@diabol.se>
2012-11-11 20:46:26 +0100

    Wrote testBoa as py.test test file

--- task3/testBoa.py
+++ task3/testBoa.py
@@ -2,31 +2,35 @@
 import socket
 import sys
  
-HOST = 'localhost'
-GET = 'faulty'
-PORT = int(sys.argv[1])
+def make_request(port): 
+	HOST = 'localhost'
+	GET = 'faulty'
+        try:
+  		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error, msg:
+	  	sys.stderr.write("[ERROR] %s\n" % msg[1])
+  		sys.exit(1)
+	try:
+  		sock.connect((HOST, port))
+	except socket.error, msg:
+  		sys.stderr.write("[ERROR] %s\n" % msg[1])
+  		sys.exit(2)
  
-try:
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(1)
+	sock.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" % (GET, HOST))
  
-try:
-  sock.connect((HOST, PORT))
-except socket.error, msg:
-  sys.stderr.write("[ERROR] %s\n" % msg[1])
-  sys.exit(2)
+	data = sock.recv(1024)
+	string = ""
+	while len(data):
+  		string = string + data
+  		data = sock.recv(1024)
+	sock.close()
+	return string
  
-sock.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" % (GET, HOST))
- 
-data = sock.recv(1024)
-string = ""
-while len(data):
-  string = string + data
-  data = sock.recv(1024)
-sock.close()
- 
-print string
- 
-sys.exit(0)
+#Check that the BAD REQUEST is part of the response.
+def test_that_9411_returns_bad_request():
+        response = make_request(9411)
+	assert response.find('400 Bad Request') > 0
+
+def test_that_9412_returns_bad_request():
+        response = make_request(9412)
+        assert response.find('400 Bad Request') > 0
